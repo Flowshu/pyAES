33a0deda9213feaa932234553c5994e5ae454121
fl0wshu <floowshu@gmail.com>
2019-10-01 23:49:11 +0200

    implement PKCS#7 padding

--- padding.py
+++ padding.py
@@ -1,8 +1,26 @@
-#! /usr/bin/env python3
+'''
+This module implements the PKCS#7 padding.
+'''
 
-block = int(input("Block Length: "))
-strng = input("Text: ")
+def apply_padding(message: bytes, block=16):
+    bytes_in_last_block = len(message) % block
+    if bytes_in_last_block != 0:
+        missing_bytes = block - bytes_in_last_block
+        padding = bytes([missing_bytes]) * missing_bytes
+        return message + padding
+    else:
+        return message
 
-to_padd = block - (len(strng) % block)
-byt = ""
-out = strng + (to_padd * byt)
+def remove_padding(message: bytes, block=16):
+    if message == b'':
+        return message
+    message_length = len(message)
+    padded_bytes = message[message_length-1]
+    if 1 <= padded_bytes <= block:
+        for padded_byte in range(padded_bytes):
+            if padded_bytes != message[message_length-1-padded_byte]:
+                return message
+            else:
+                return message[:message_length-padded_bytes]
+    else:
+        return message
